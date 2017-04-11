0b8eaaec730ceb068ce0dc700305ede54b61abc3
Jason Boxman <jasonb@edseek.com>
2017-04-11 17:47:05 -0400

    Checkpoint

--- models/poll.js
+++ models/poll.js
@@ -16,12 +16,36 @@ const pollSchema = mongoose.Schema({
     type: Number,
     default: 0
   },
+  // use validate and fn to enforce not empty, but what about message?
   answers: [answerSchema]
 });
 
 pollSchema.statics.findAllPolls = function findAllPolls() {
   return this.find({});
 }
+
+// What happens if one or more answers is invalid?
+pollSchema.statics.addPoll = function addPoll(o) {
+  // Get back err.errors on failure.
+  return this.create(o);
+}
+
+pollSchema.statics.removePoll = function removePoll(id) {
+  return this.findByIdAndRemove(id).exec();
+}
+
+/*
+  TODO
+  - Increment view count
+*/
+pollSchema.statics.viewPoll = function viewPoll(id) {
+  return this.findById(id).exec();
+}
+
+// Use pollSchema.methods.blah to allow for custom methods
+
+module.exports = mongoose.model('Poll', pollSchema);
+
 // We need back validation failure
 /*
 { [ValidationError: Validation failed]
@@ -36,19 +60,3 @@ pollSchema.statics.findAllPolls = function findAllPolls() {
         type: 'required',
         value: undefined } } }
 */
-pollSchema.statics.addPoll = function addPoll(o) {
-  // Get back err.errors on failure.
-  return this.create(o);
-}
-
-pollSchema.statics.removePoll = function removePoll(id) {
-  return this.findByIdAndRemove(id).exec();
-}
-
-pollSchema.statics.viewPoll = function viewPoll(id) {
-  return this.findById(id).exec();
-}
-
-// Use pollSchema.methods.blah to allow for custom methods
-
-module.exports = mongoose.model('Poll', pollSchema);
