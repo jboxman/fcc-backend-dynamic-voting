01a5f14ae1f9aedd17dd6e328dca2aae1a530193
Jason Boxman <jasonb@edseek.com>
2017-07-19 15:38:00 -0400

    Use Semantic UI

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,13 +6,38 @@
 // When you add this file, we won't add the default configurations which is similar
 // to "React Create App". This only has babel loader to load JavaScript.
 
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+// webpack.config.dev.js -- from react-slingshot
 module.exports = {
   plugins: [
-    // your custom plugins
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true, // set to false to see a list of every file being bundled.
+      options: {
+        sassLoader: {
+          includePaths: [path.resolve(__dirname, 'frontend/', 'scss')]
+        },
+        context: '/',
+        postcss: () => [autoprefixer],
+      }
+    })
   ],
   module: {
-    loaders: [
-      // add your custom loaders.
-    ],
-  },
+    rules: [
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
+      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
+      {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
+      {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']
+      }
+    ]
+  }
 };
