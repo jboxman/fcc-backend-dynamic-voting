6d412b9c1f5f80dabe71a2a8907da161276c4597
Jason Boxman <jasonb@edseek.com>
2017-07-20 11:37:05 -0400

    Minor cleanup

--- frontend/index.js
+++ frontend/index.js
@@ -4,21 +4,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import Root from './App/Root';
 
 import configureStore from './App/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+//import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
-
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
 
 render(
   <AppContainer>
