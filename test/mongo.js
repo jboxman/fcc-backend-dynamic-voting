1048305dbb4ef772f9cb1ad066d0d9e5494ff9f4
Jason Boxman <jasonb@edseek.com>
2017-08-10 22:49:56 -0400

    Cleanup

--- test/mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test');
-
-const db = mongoose.connection;
-db.once('open', () => {
-    console.log('I am connected.');
-});
