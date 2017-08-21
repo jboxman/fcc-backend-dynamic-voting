3c30054731e1c99bc20b5fcb31c853958c370907
Jason Boxman <jasonb@edseek.com>
2017-08-21 19:29:36 -0400

    Connect user login

--- frontend/components/AppMenu.js
+++ frontend/components/AppMenu.js
@@ -1,34 +1,57 @@
+const config = require('../../config/envs');
+
 import React from 'react';
 import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 
 import {
   Input,
   Menu
 } from 'semantic-ui-react';
 
-/*
-  We care about user authentication status here.
-  isAuthenticated?
-*/
+import popup from '../../util/openWindow';
+import * as userActions from '../redux/userActions';
 
 class AppMenu extends React.Component {
   constructor() {
       super();
       this.state = {};
-      this.handleItemClick = this.handleItemClick.bind(this);
+      // This mysteriously fails with
+      // warning: setstate(...): can only update a mounted or mounting component
+      //this.handleItemClick = this.handleItemClick.bind(this);
+      //console.log(this);
+  }
+
+  // dispatch to user login
+  handleLogIn(e, {name}) {
+    const promise = popup(config.app.oauthUrl);
+    promise.then(user => this.props.actions.injectUser(user));
+  }
+
+  handleLogOut(e, {name}) {
+    this.props.actions.logoutUser();
   }
 
   handleItemClick(e, {name}) {
+    console.log(name);
     this.setState({activeItem: name});
   }
 
   render() {
-    const { activeItem } = this.state;
+    const {activeItem} = this.state;
+    const {isAuthenticated} = this.props;
+
+    const loginButton =  isAuthenticated ?
+      <Menu.Item name="Logout" onClick={this.handleLogOut.bind(this)} />
+      : 
+      <Menu.Item name="Login" onClick={this.handleLogIn.bind(this)} />;
+
     return (
       <Menu>
         <Menu.Item
           name='Add poll'
-          onClick={this.handleItemClick}
+          onClick={this.handleItemClick.bind(this)}
         >
           Add poll
         </Menu.Item>
@@ -48,7 +71,7 @@ class AppMenu extends React.Component {
           All polls
         </Menu.Item>
         <Menu.Menu position='right'>
-          <Menu.Item name="Login" />
+          {loginButton}
         </Menu.Menu>
       </Menu>
     )
@@ -59,4 +82,11 @@ AppMenu.defaultProps = {
   isAuthenticated: true
 }
 
-export default AppMenu;
+const mapStateToProps = state => ({
+  isAuthenticated: state.currentUser.isAuthenticated
+});
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(userActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppMenu);
