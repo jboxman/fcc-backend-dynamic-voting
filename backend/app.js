f65b21d1a97f14de2a1723e51909cf8b40266e9f
Jason Boxman <jasonb@edseek.com>
2017-08-31 15:27:00 -0400

    Use different static serve middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const passport = require('koa-passport');
 const session = require('koa-session');
 const logger = require('koa-logger')
+const static = require('koa-static');
 
 const routes = require('./routes');
 
@@ -14,12 +16,23 @@ app.keys = ['a secret'];
 
 app.use(logger());
 app.use(bodyParser());
+// This can vary based on environment
 app.use(session({}, app));
+
+const views = require('koa-views');
+app.use(views(`${__dirname}/views`, {
+  map: {
+    html: 'ejs'
+  }
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+// TODO - use koa-mount so /api works in PRODUCTION
 app.use(routes.routes());
 app.use(routes.allowedMethods());
+
+app.use(static(path.join(__dirname, '../dist')));
 
 module.exports = app;
