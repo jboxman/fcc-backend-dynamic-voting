63f587e15e0de6747cee995d7990e1efd9c4e77d
Jason Boxman <jasonb@edseek.com>
2017-09-01 15:28:40 -0400

    Fix mount path

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // TODO - use koa-mount so /api works in PRODUCTION
-app.use(mount(process.env.NODE_ENV == 'production' ? '/api' : '', routes.routes()));
+app.use(mount(process.env.NODE_ENV == 'production' ? '/api' : '/', routes.routes()));
 app.use(routes.allowedMethods());
 
 app.use(static(path.join(__dirname, '../dist')));
