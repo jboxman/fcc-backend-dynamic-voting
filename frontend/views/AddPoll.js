3f6be4c256dbf4ef39498cc8e836b6d8059d1a89
Jason Boxman <jasonb@edseek.com>
2017-09-12 21:42:46 -0400

    Update AddPoll view

--- frontend/views/AddPoll.js
+++ frontend/views/AddPoll.js
@@ -4,6 +4,30 @@ import { connect } from 'react-redux';
 
 import PollForm from '../components/PollForm';
 
-export default function(props) {
-  return <PollForm />;
+export default class AddPoll extends React.Component {
+
+  handleCancel() {
+    this.props.router.push('/');
+  }
+
+  doRedirect() {
+    this.props.router.push('/');
+  }
+
+  render() {
+    return <PollForm
+      handleCancel={this.handleCancel.bind(this)}
+      doRedirect={this.doRedirect.bind(this)}
+      initialData={this.props.initialData}
+      />;
+  }  
+}
+
+AddPoll.defaultProps = {
+  initialData: {
+    question: '',
+    answers: [
+      '', '', '', '', '', ''
+    ]
+  }
 };
